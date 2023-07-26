b17d9e49ce23e45e1d19d3a162ddd83d6bcdc2d5
yash <yashj7911@gmail.com>
2023-07-26 21:22:57 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,27 @@
+let movies =[
+    {
+    name:'falcon and the winter solidier',
+    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laboriosam deleniti perspiciatis laborum impedit facere',
+    image:'./hotstar-images/slider 2.PNG '
+},
+{
+    name:'luca',
+    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laboriosam deleniti perspiciatis laborum impedit facere',
+    image:'./hotstar-images/slider 5.PNG '
+},
+{
+    name:'loki',
+    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laboriosam deleniti perspiciatis laborum impedit facere',
+    image:'./hotstar-images/slider 1.PNG '
+},
+{
+    name:'wanda vision',
+    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laboriosam deleniti perspiciatis laborum impedit facere',
+    image:'./hotstar-images/slider 3.PNG'
+},
+{
+    name:'raya and the last dragon',
+    des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus laboriosam deleniti perspiciatis laborum impedit facere',
+    image:'./hotstar-images/slider 4.PNG'
+}
+]
